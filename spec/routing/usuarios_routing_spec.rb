d95646c143258fa7d7b3282909305f0457adb0d7
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-04 15:17:37 -0300

    fix: editar usuarios_routing_spec.rb para o REST
    editar o teste de rotas usuarios_routing_spec.rb para se adequar ao esquema de rotas estabelecido pelo padrão REST

--- spec/routing/usuarios_routing_spec.rb
+++ spec/routing/usuarios_routing_spec.rb
@@ -1,41 +1,43 @@
 require "rails_helper"
 
-RSpec.describe "rotas para usuarios" do
-    it "rota /usuarios para controlador usuario#index" do
-        expect(get("/usuarios")).to route_to("usuario#index")
-    end
-    context "listagem de usuarios" do
-        it "rota /usuarios/listagem para controlador usuario#listagem" do
-            expect(get("/usuarios/listagem")).to route_to("usuario#listagem")
+RSpec.describe "Testes de rotas para usuarios" do
+    context "Testes de rotas de login de usuário" do
+        it "rota POST /autenticacao/login para controlador autenticacao#login" do
+            expect(post "/autenticacao/login").to route_to("autenticacao#login")
         end
     end
-    context "exibição individual de usuário" do
-        it "rota /usuarios/1 para controlador usuario#exibir" do
-            expect(get("/usuarios/1")).to route_to("usuario#exibir", id: "1")
-        end
-        it "rota /usuarios/0 não deve ser acessível" do
-            expect(get("/usuarios/0")).not_to be_routable
+    context "Testes de rotas de listagem de usuarios" do
+        it "rota GET /usuarios para controlador usuario#index" do
+            expect(get "/usuarios").to route_to("usuario#index")
         end
     end
-    context "criação de usuário" do
-        it "rota /usuarios/criacao para controlador usuario#criacao" do
-            expect(get("/usuarios/criacao")).to route_to("usuario#criacao")
+    context "Testes de rotas de exibição individual de usuário" do
+        it "rota GET /usuarios/1 para controlador usuario#show" do
+            expect(get "/usuarios/1").to route_to("usuario#show", id: "1")
+        end
+        it "rota GET /usuarios/0 não deve ser acessível" do
+            expect(get "/usuarios/0").not_to be_routable
         end
     end
-    context "edição de usuário" do
-        it "rota /usuarios/editar/1 para controlador usuario#editar" do
-            expect(get("/usuarios/editar/1")).to route_to("usuario#editar", id: "1")
-        end
-        it "rota /usuarios/editar/0 não deve ser acessível" do
-            expect(get("/usuarios/editar/0")).not_to be_routable
+    context "Testes de rotas de criação de usuário" do
+        it "rota POST /usuarios para controlador usuario#create" do
+            expect(post "/usuarios").to route_to("usuario#create")
         end
     end
-    context "exclusão de usuário" do
-        it "rota /usuarios/excluir/1 para controlador usuario#excluir" do
-            expect(get("/usuarios/excluir/1")).to route_to("usuario#excluir", id: "1")
+    context "Testes de rotas de edição de usuário" do
+        it "rota PUT /usuarios/1 para controlador usuario#edit" do
+            expect(put "/usuarios/1").to route_to("usuario#update", id: "1")
         end
-        it "rota /usuarios/excluir/0 não deve ser acessível" do
-            expect(get("/usuarios/excluir/0")).not_to be_routable
+        it "rota PUT /usuarios/0 não deve ser acessível" do
+            expect(put "/usuarios/0").not_to be_routable
+        end
+    end
+    context "Testes de rotas de exclusão de usuário" do
+        it "rota DELETE /usuarios/1 para controlador usuario#destroy" do
+            expect(delete "/usuarios/1").to route_to("usuario#destroy", id: "1")
+        end
+        it "rota DELETE /usuarios/0 não deve ser acessível" do
+            expect(delete "/usuarios/0").not_to be_routable
         end
     end
 end
