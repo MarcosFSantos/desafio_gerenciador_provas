c77cf7d14944a5f99007a7aa364b978db34adba3
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-04 02:07:45 -0300

    feat: configurar pasta spec/routing no rspec
    configurar arquivo rails_helper para permitir a criação pasta de teste spec/routing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,9 @@ RSpec.configure do |config|
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
   #
+
+  config.pattern = "spec/routing/**/*_spec.rb"
+
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
