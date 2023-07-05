e9ce3aef1928d62237a8d51bf10f535386321969
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-05 19:38:38 -0300

    refactor: mudar nome das ações de usuarios
    mudar os nomes das ações do controlador usuarios_controller para nomes mais intuitivos

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,27 +1,27 @@
 class UsuariosController < ApplicationController
 
     # Método de listgem de usuários
-    def index
+    def listar
         render json: {pagina: 'Você está na página de listagem de usuário'}, status: :ok
     end
 
     # Método de exibição de um usuário
-    def show
+    def mostrar
         render json: {pagina: 'Você está na página de exibição de um usuário'}, status: :ok
     end
     
     # Método de criação de um usuário
-    def create
+    def criar
         render json: {pagina: 'Você está na página de criação de um usuário'}, status: :ok
     end
     
     # Método de edição de um usuário
-    def update
+    def atualizar
         render json: {pagina: 'Você está na página de edição de um usuário'}, status: :ok
     end
     
     # Método de exclusão de um usuário
-    def destroy
+    def destruir
         render json: {pagina: 'Você está na página de exclusão de um usuário'}, status: :ok
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/usuarios", to: "usuarios#index"
-  post "/usuarios", to: "usuarios#create"
-  get "/usuarios/:id", to: "usuarios#show", constraints: { id: /[1-9]\d*/ }
-  put "/usuarios/:id", to: "usuarios#update", constraints: { id: /[1-9]\d*/ }
-  delete "/usuarios/:id", to: "usuarios#destroy", constraints: { id: /[1-9]\d*/ }
+  get "/usuarios", to: "usuarios#listar"
+  post "/usuarios", to: "usuarios#criar"
+  get "/usuarios/:id", to: "usuarios#mostrar", constraints: { id: /[1-9]\d*/ }
+  put "/usuarios/:id", to: "usuarios#atualizar", constraints: { id: /[1-9]\d*/ }
+  delete "/usuarios/:id", to: "usuarios#destruir", constraints: { id: /[1-9]\d*/ }
   post "/autenticacao/login", to: "autenticacao#login"
 end
