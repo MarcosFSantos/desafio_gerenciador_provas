3f3449ad655eb11c7f572ede9bd0c0b3a1690639
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-05 00:36:56 -0300

    feat: criar defs dos controllers de usuarios
    criar ações ou defs dos controladores usuarios e atenticacao

--- app/controllers/autenticacao_controller.rb
+++ app/controllers/autenticacao_controller.rb
@@ -1,2 +1,7 @@
 class AutenticacaoController < ApplicationController
+
+    # Método de exibição da página de login
+    def login
+        render json: {pagina: 'Você está na página de login'}, status: :ok
+    end
 end
--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,2 +1,27 @@
 class UsuariosController < ApplicationController
+
+    # Método de listgem de usuários
+    def index
+        render json: {pagina: 'Você está na página de listagem de usuário'}, status: :ok
+    end
+
+    # Método de exibição de um usuário
+    def show
+        render json: {pagina: 'Você está na página de exibição de um usuário'}, status: :ok
+    end
+    
+    # Método de criação de um usuário
+    def create
+        render json: {pagina: 'Você está na página de criação de um usuário'}, status: :ok
+    end
+    
+    # Método de edição de um usuário
+    def update
+        render json: {pagina: 'Você está na página de edição de um usuário'}, status: :ok
+    end
+    
+    # Método de exclusão de um usuário
+    def destroy
+        render json: {pagina: 'Você está na página de exclusão de um usuário'}, status: :ok
+    end
 end
