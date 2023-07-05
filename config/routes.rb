bdb2ab347298c46819a2f2e65508f456e44639a2
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-05 19:07:32 -0300

    feat: alterar rotas para não permitir id zero
    alterar rotas de usuario para que as rotas "GET /usuarios/0", "PUT /usuarios/0" e "DELETE /usuarios/0" não sejam permitidas pois contêm o id de usuario igual a zero

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :usuarios
-  post "/autenticacao/login" => "autenticacao#login"
+  get "/usuarios", to: "usuarios#index"
+  post "/usuarios", to: "usuarios#create"
+  get "/usuarios/:id", to: "usuarios#show", constraints: { id: /[1-9]\d*/ }
+  put "/usuarios/:id", to: "usuarios#update", constraints: { id: /[1-9]\d*/ }
+  delete "/usuarios/:id", to: "usuarios#destroy", constraints: { id: /[1-9]\d*/ }
+  post "/autenticacao/login", to: "autenticacao#login"
 end
