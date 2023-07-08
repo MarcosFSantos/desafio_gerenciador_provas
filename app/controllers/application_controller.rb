9f750d95396a62aa0714224ac151b605e9933be5
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-08 16:40:58 -0300

    feat: criar funcao autorizar_requisicao no controlador application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+    def autorizar_requisicao
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decodificado = JsonWebTokenUtilitarios.decodificar(header)
+            if @decodificado != nil
+                @usuario_atual = Usuario.find(@decodificado[:id_usuario])
+            else
+                render json: { errors: 'Token não encontrado' }, status: :unauthorized
+            end
+        rescue ActiveRecord::RecordNotFound => exception
+            render json: { errors: exception.message }, status: :unauthorized
+        rescue JWT::DecodeError => exception
+            render json: { errors: exception.message }, status: :unauthorized
+        end
+    end
+    
 end
