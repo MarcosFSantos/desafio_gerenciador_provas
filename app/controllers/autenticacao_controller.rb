a2058c708dcc1e2593fc3b1d0aab84ea35f3f9da
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-08 16:52:52 -0300

    feat: implementar o controller autenticacao
    implementar o controller autenticacao para funcionar com o JWT

--- app/controllers/autenticacao_controller.rb
+++ app/controllers/autenticacao_controller.rb
@@ -1,7 +1,22 @@
 class AutenticacaoController < ApplicationController
+    before_action :autorizar_requisicao, except: :login
 
     # Método de exibição da página de login
     def login
-        render json: {pagina: 'Você está na página de login'}, status: :ok
+        @usuario = Usuario.find_by_email(params[:email])
+        if @usuario&.authenticate(params[:password])
+            token = JsonWebTokenUtilitarios.codificar(id_usuario: @usuario.id)
+            tempo = Time.now + 24.hours.to_i
+            render json: { token: token, expiracao: tempo.strftime("%m-%d-%Y %H:%M"), matricula: @usuario.matricula }, status: :ok
+        else
+            render json: { error: 'unauthorized' }, status: :unauthorized
+        end
     end
+
+   private
+
+   def parametros_login
+    params.permit(:email, :password)
+   end
+   
 end
