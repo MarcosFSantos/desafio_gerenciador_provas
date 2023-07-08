93f245a1fcfe15662192f448bad0e1a142af95cd
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-08 16:42:44 -0300

    feat: implementar o controlador applicatiion
    implementar as funcoes listar, mostrar, criar, atualizar, destruir, encontrar_usuario e parametros_usuario no controlador UsuariosController

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,27 +1,49 @@
 class UsuariosController < ApplicationController
+    before_action :autorizar_requisicao, except: :criar
+    before_action :encontrar_usuario, except: %i[criar listar]
 
     # Método de listgem de usuários
     def listar
-        render json: {pagina: 'Você está na página de listagem de usuário'}, status: :ok
+        @usuarios = Usuario.all
+        render json: @usuarios, status: :ok
     end
 
     # Método de exibição de um usuário
     def mostrar
-        render json: {pagina: 'Você está na página de exibição de um usuário'}, status: :ok
+        render json: @usuario, status: :ok
     end
     
     # Método de criação de um usuário
     def criar
-        render json: {pagina: 'Você está na página de criação de um usuário'}, status: :ok
+        @usuario = Usuario.new(parametros_usuario)
+        if @usuario.save
+            render json: @usuario, status: :ok
+        else
+            render json: { errors: @usuario.errors.full_messages },status: :unprocessable_entity
+        end
     end
     
     # Método de edição de um usuário
     def atualizar
-        render json: {pagina: 'Você está na página de edição de um usuário'}, status: :ok
+        unless @usuario.update(parametros_usuario)
+            render json: { errors: @usuario.errors.full_messages }, status: :ok
+        end
     end
     
     # Método de exclusão de um usuário
     def destruir
-        render json: {pagina: 'Você está na página de exclusão de um usuário'}, status: :ok
+        @usuario.destroy
+    end
+
+    private
+
+    def encontrar_usuario
+        @usuario = Usuario.find_by_matricula!(params[:matricula])
+        rescue ActiveRecord::RecordNotFound
+            render json: { errors: 'Usuários não encontrado' }, status: :not_found
+    end
+
+    def parametros_usuario
+        params.permit(:nome, :email, :matricula, :password, :password_confirmation)
     end
 end
