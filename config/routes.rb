184f8ad5fbc9a05231b37c4291ea75a7d0d02dd2
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-09 04:46:00 -0300

    fix: consertar rotas de usuario
    consertar rotas de usuario para que a rotas exigam a matricula ao invés do id

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/usuarios", to: "usuarios#listar"
   post "/usuarios", to: "usuarios#criar"
-  get "/usuarios/:id", to: "usuarios#mostrar", constraints: { id: /[1-9]\d*/ }
-  put "/usuarios/:id", to: "usuarios#atualizar", constraints: { id: /[1-9]\d*/ }
-  delete "/usuarios/:id", to: "usuarios#destruir", constraints: { id: /[1-9]\d*/ }
+  get "/usuarios/:matricula", to: "usuarios#mostrar", constraints: { id: /[1-9]\d*/ }
+  put "/usuarios/:matricula", to: "usuarios#atualizar", constraints: { id: /[1-9]\d*/ }
+  delete "/usuarios/:matricula", to: "usuarios#destruir", constraints: { id: /[1-9]\d*/ }
   post "/autenticacao/login", to: "autenticacao#login"
 end
