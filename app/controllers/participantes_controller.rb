4053ef6ea97eb13b48f40ebf422bb588373c3684
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-11 14:59:01 -0300

    feat: criar participantes_controller

--- app/controllers/participantes_controller.rb
+++ app/controllers/participantes_controller.rb
@@ -1,2 +1,26 @@
 class ParticipantesController < ApplicationController
+    # Método de listgem de participante
+    def listar
+
+    end
+
+    # Método de exibição de um participante
+    def mostrar
+
+    end
+    
+    # Método de criação de um participante
+    def criar
+
+    end
+    
+    # Método de edição de um participante
+    def atualizar
+
+    end
+    
+    # Método de exclusão de um participante
+    def destruir
+        
+    end
 end
