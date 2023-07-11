598647db729a7991cd5e109e3c03db7e9b9311f9
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-11 20:55:51 -0300

    feat: gerar model prova

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_230645) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_235343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_230645) do
     t.datetime "updated_at", null: false
     t.bigint "usuarios_id"
     t.index ["usuarios_id"], name: "index_participantes_on_usuarios_id"
+  end
+
+  create_table "provas", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "usuarios", force: :cascade do |t|
