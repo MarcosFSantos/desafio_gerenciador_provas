e9afd992fffef9ebb1a21ceb8445507fc528df17
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-11 14:36:11 -0300

    refactor: remover restrições das rotas de usuarios
    remover restrições desnecessárias de id das rotas de usuários, pois os usuários utilizam matricula nas rotas que podem ter o valor zero

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/usuarios", to: "usuarios#listar"
   post "/usuarios", to: "usuarios#criar"
-  get "/usuarios/:matricula", to: "usuarios#mostrar", constraints: { id: /[1-9]\d*/ }
-  put "/usuarios/:matricula", to: "usuarios#atualizar", constraints: { id: /[1-9]\d*/ }
-  delete "/usuarios/:matricula", to: "usuarios#destruir", constraints: { id: /[1-9]\d*/ }
+  get "/usuarios/:matricula", to: "usuarios#mostrar"
+  put "/usuarios/:matricula", to: "usuarios#atualizar"
+  delete "/usuarios/:matricula", to: "usuarios#destruir"
   post "/autenticacao/login", to: "autenticacao#login"
   get "/participantes", to: "participantes#listar"
   get "/participantes/:id", to: "participantes#mostrar", constraints: { id: /[1-9]\d*/ }
