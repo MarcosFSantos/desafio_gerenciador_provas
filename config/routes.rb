df2651e37573cb4d0a6ea5c114cf8606186a32d1
MarcosFSantos <marcos.franca1673@gmail.com>
2023-07-11 14:35:06 -0300

    feat: criar rotas de participante

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   put "/usuarios/:matricula", to: "usuarios#atualizar", constraints: { id: /[1-9]\d*/ }
   delete "/usuarios/:matricula", to: "usuarios#destruir", constraints: { id: /[1-9]\d*/ }
   post "/autenticacao/login", to: "autenticacao#login"
+  get "/participantes", to: "participantes#listar"
+  get "/participantes/:id", to: "participantes#mostrar", constraints: { id: /[1-9]\d*/ }
+  post "/participantes", to: "participantes#criar"
+  put "/participantes/:id", to: "partipantes#atualizar", constraints: { id: /[1-9]\d*/ }
+  delete "/participantes/:id", to: "participantes#destruir", constraints: { id: /[1-9]\d*/ }
 end
